310af1bb5a92be25080d0e274ac621369ff410cc
ghegadmalakshay-roman <ghegadmal.akshay@gmail.com>
2026-01-05 14:49:23 +0530

    Delete kli_demo/employee-management directory

--- kli_demo/employee-management/src/app/model/class/employee-form-request.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmployeeFormRequest } from './employee-form-request';
-
-describe('EmployeeFormRequest', () => {
-  it('should create an instance', () => {
-    expect(new EmployeeFormRequest()).toBeTruthy();
-  });
-});
